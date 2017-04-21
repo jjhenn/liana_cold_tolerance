3d7e09a9cf9fb10a5ff68ff5c3c1fbf566b14a43
JJHenn <henn2@wisc.edu>
2017-04-21 10:37:13 -0500

    hello

--- data_organization.R
+++ data_organization.R
@@ -1,5 +1,6 @@
 ## Dataset creation for cold tolerance data
 library(dplyr)
+library(tidyr)
 
 xy <- read.csv("liana_xy.csv")
 supercooling <- read.csv("liana_supercooling.csv")
@@ -11,8 +12,34 @@ supercooling <- supercooling %>%
   mutate(sample_id = paste(population, rep, leaf, sep = "")) %>% 
   select(-13)
 
-cold_tol <- leaf_area %>% 
-  full_join(PEL_lt50, by = "sample_id") %>% 
-  full_join(supercooling, by = "sample_id") %>% 
-  full_join(leaf_mass, by = "sample_id") %>% 
-  full_join(xy, by = "population")
+samples <- leaf_mass %>% 
+  full_join(leaf_area) %>% 
+  full_join(PEL_lt50) %>% 
+  #full_join(supercooling, by = "sample_id") %>% 
+  distinct(sample_id)
+
+samples = read.csv("samples.csv")
+
+cold_tol <- samples %>% 
+  full_join(PEL_lt50) %>% 
+  full_join(leaf_mass) %>% 
+  full_join(supercooling) %>% 
+  full_join(leaf_area) %>% 
+  full_join(xy, by = "population") %>% 
+  select(-spp.x) %>% 
+  filter(PEL_LT50 < 0) %>% 
+  mutate(SLA = (area_cm2/(dry_mass/1000))) %>% 
+  mutate(sc_LDMC = (sc_wet_mass/sc_dry_mass)) %>% 
+  mutate(LDMC = (wet_mass/dry_mass)) %>% 
+  filter(spp.y != "PATR")
+
+
+
+
+plot(PEL_LT50 ~ wet_mass, data = cold_tol, col = spp.y)
+
+
+test <- lmer(PEL_LT50 ~ latitude + spp.y + area_cm2 + latitiude:spp.y + 
+             latitiude:area_cm2 + spp.y:area_cm2 + (1|population), data = cold_tol)
+
+
