c59ac0795dfc9ca63b45084120375dec2a414d28
JJHenn <henn2@wisc.edu>
2017-05-02 07:30:45 -0500

    update

--- bioclim_data.R
+++ bioclim_data.R
@@ -31,6 +31,7 @@ liana_bioclim <- liana_bioclim %>%
 colnames(liana_bioclim) <- c("population", "latitude", "longitude", "spp", "bio1", "bio2", "bio3", "bio4", "bio5", "bio6", "bio7", "bio8", "bio9", "bio10", "bio11", "bio12", "bio13", "bio14", "bio15", "bio16", "bio17", "bio18", "bio19")
 
 write.csv(liana_bioclim, "liana_bioclim.csv")
+liana_bioclim <- read.csv("liana_bioclim.csv")
 
 plot(bio11 ~ latitude, data = liana_bioclim)
-pairs(liana_bioclim[,c(2,5:23)])
+pairs(liana_bioclim[,c(3,4,6:24)])
--- data_organization.R
+++ data_organization.R
@@ -3,12 +3,14 @@
 ## Dataset creation for cold tolerance data
 library(dplyr)
 library(tidyr)
+library(lmerTest)
 
 xy <- read.csv("liana_xy.csv")
 supercooling <- read.csv("liana_supercooling.csv")
 PEL_lt50 <- read.csv("liana_pel_lt50.csv")
 leaf_area <- read.csv("liana_leaf_area.csv")
 leaf_mass <- read.csv("liana_leaf_mass.csv")
+bioclim <- read.csv("liana_bioclim.csv")
 
 supercooling <- supercooling %>% 
   mutate(sample_id = paste(population, rep, leaf, sep = "")) %>% 
@@ -20,28 +22,44 @@ samples <- leaf_mass %>%
   #full_join(supercooling, by = "sample_id") %>% 
   distinct(sample_id)
 
-samples = read.csv("samples.csv")
+samples = read.csv("samples.csv") %>% 
+  select(-X)
 
-cold_tol <- samples %>% 
+
+
+PEL <- samples %>% 
   full_join(PEL_lt50) %>% 
   full_join(leaf_mass) %>% 
-  full_join(supercooling) %>% 
+  #full_join(supercooling) %>% 
   full_join(leaf_area) %>% 
   full_join(xy, by = "population") %>% 
+  full_join(bioclim) %>% 
   select(-spp.x) %>% 
   filter(PEL_LT50 < 0) %>% 
   mutate(SLA = (area_cm2/(dry_mass/1000))) %>% 
-  mutate(sc_LDMC = (sc_wet_mass/sc_dry_mass)) %>% 
-  mutate(LDMC = (wet_mass/dry_mass)) %>% 
-  filter(spp.y != "PATR")
+  mutate(LDMC = (dry_mass/wet_mass)) %>% 
+  filter(spp != "PATR")
+
+sc <- samples %>% 
+  full_join(supercooling) %>%
+  full_join(xy, by = "population") %>% 
+  full_join(bioclim) %>% 
+  mutate(ldmc = sc_dry_mass/sc_wet_mass) %>% 
+  filter(spp != "PATR") %>% 
+  filter(sc_wet_mass > 0) %>% 
+  filter(spp != "VIRI")
+
+plot(PEL_LT50 ~ bio6, data = PEL, col = spp.y)
 
 
+test <- lmer(PEL_LT50 ~ latitude*spp + spp*LDMC + (1|population)
+             , data = PEL)
+s.test <- step(test)
 
+means <- PEL %>% 
+  group_by(population) %>% 
+  summarize (mean = mean(PEL_LT50), ldmc = mean(LDMC), dry = mean(dry_mass)) %>% 
+  full_join(xy)
 
-plot(PEL_LT50 ~ wet_mass, data = cold_tol, col = spp.y)
-
-
-test <- lmer(PEL_LT50 ~ latitude + spp.y + area_cm2 + latitiude:spp.y + 
-             latitiude:area_cm2 + spp.y:area_cm2 + (1|population), data = cold_tol)
-
-
+plot(ldmc ~ latitude, col = spp, data = means)
+test <- lm(log(-mean) ~ latitude*spp + spp*ldmc, data = means)
